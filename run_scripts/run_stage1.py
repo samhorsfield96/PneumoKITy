770356763436293127c6f313acf03af50d6cf178
Sam Horsfield <57448593+samhorsfield96@users.noreply.github.com>
2024-01-03 17:04:59 +0000

    Update run_stage1.py
    Corrects issue with pandas 2.0

--- run_scripts/run_stage1.py
+++ run_scripts/run_stage1.py
@@ -52,7 +52,8 @@ def translate_mixmm(mix_mm, session):
     df = pd.DataFrame(columns=["serotype", "mm"], dtype=object)
     for hit in mix_keys:
         pheno = list(get_pheno_list([hit], session))[0]
-        df = df.append({"serotype": pheno, "mm": mix_mm[hit]}, ignore_index=True, sort=False)
+        new_row = pd.DataFrame({"serotype": [pheno], "mm": [mix_mm[hit]]})
+        df = pd.concat([df, new_row], ignore_index=True, sort=False)
 
     # group dataframe keeping max mm value for duplicates
     df = df.groupby("serotype", as_index=False).max()
